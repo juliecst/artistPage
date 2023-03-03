b5152cea9c3bc34dc91b8a8c96533e3120f264dd
juliecst <julia_stamm@yahoo.de>
2023-03-03 13:31:34 +0000

    added celian#

--- sth.js
+++ sth.js
@@ -6,7 +6,8 @@
         'https://github.com/juliecst/annaRoom/settings/pages',
         'https://www.window-swap.com/Window',
         'https://www.bbc.co.uk/news/uk-politics-64815875',
-        'https://juliecst.github.io/annaRoom/'
+        'https://juliecst.github.io/annaRoom/',
+        'https://juliecst.github.io/celiaRoom/'
     
       ];
     var site = sites[Math.floor(Math.random() * sites.length)];
